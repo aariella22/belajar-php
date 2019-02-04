ec0a410411802f443516bd386b8bda84e77384bc
aariella22 <46362798+aariella22@users.noreply.github.com>
2019-02-04 09:15:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Boolean atau True Atau Flase ; True = 1 & False = 0 //
+// Integer Atau Bisa Disebut Angka dan Bilangan Bulat, 24 25 62 34 //
+// Floating Point Number(float), Atau disebut decimal 100.24 atau 24.5 //
+// Array adalah campuran Tipe Data Secara kompleks ['Ariella', 'Giovanni'] //
+// Null , Bukan 0 tapi Null //
