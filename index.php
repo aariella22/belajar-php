1d104b0f73676c98a46c77a836db1c79a2ab2722
Ariella Giovanni Wahyuriyanto <46362798+aariella22@users.noreply.github.com>
2019-02-08 10:14:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+//integer = Angka Bulat 500 1000 0 -500 -1000//
+$myInteger = 5000;
+
+echo $myInteger;
+
+//float = Angka Decimal 500.2 -2.5 -6.3//
+$myFloat = 2.5;
+
+echo $myFloat;
+
+//Example//
+$pi = 3.14;
+
+echo $pi;
+*/
+
+//---Another Test---//
+
+/*
+//From This//
+$myMoney = 1000000;
+
+echo $myMoney;
+*/
+//To This//
+$myMoney = 1e6;
+
+echo $myMoney
+
+?>
