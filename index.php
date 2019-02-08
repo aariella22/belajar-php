8a30005819b2bde42a3b8252ace003760dae9c60
Ariella Giovanni Wahyuriyanto <46362798+aariella22@users.noreply.github.com>
2019-02-08 10:52:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*--- BOOLEAN adalah Statement Yang Memiliki True atau False ---*/
+
+/*$isAdmin = 1;//Statement 1 atau 0 adalah True Atau False//
+
+if ($isAdmin) {
+    echo 'Kamu Adalah Admin.';
+}*/
+
+
+/*
+$tebakan = is_int(5);
+
+if ($tebakan) {
+    echo "Ini Sebuah Integer";
+} else {
+    echo "Ini Sebuah String";
+}
+*/
+
+var_dump(is_int(1));
+var_dump(is_int('Ariel'));
+
+?>
