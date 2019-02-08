0e4923529c103e2af4426b518c1d6e9333233e11
aariella22 <46362798+aariella22@users.noreply.github.com>
2019-02-08 09:42:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = "Ariel";
+
+$singleCode = 'It\'s Rainy Day';
+
+$doubleCode = "It's Summer Season";
+
+$somethingDifferent = "Ariel Said \"PHP Is Fun\".";
+
+$anotherEdit = 'Ariel Said "It\'s Single Code".';
+
+$message = $name . ' Said "This Is Multi String"';
+
+echo $singleCode;
+
+echo $anotherEdit;
+
+echo $doubleCode;
+
+echo $somethingDifferent;
+
+echo $message;
+
+echo '<a href="http://google.com/"> Link </a>';
+
+?>
